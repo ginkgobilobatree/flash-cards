0e03a81ee0bce1409f6a384d3f7546d3c8dbf482
Gino Massalski <gino.massalski@gmail.com>
2021-12-06 15:23:04 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Viewer from "./components/Viewer";
+import Editor from "./components/Editor";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [cards, setCard] = useState([]);
+
+  /* ------------------------------- RETURN */
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+          <Route
+            path="viewer"
+            element={<Viewer cards={cards} setCard={setCard} />}
+          />
+          <Route
+            path="editor"
+            element={<Editor cards={cards} setCard={setCard} />}
+          />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
-
-export default App;
